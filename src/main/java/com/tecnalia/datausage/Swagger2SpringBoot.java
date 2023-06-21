2e0fb00230cb0f5fd919a6c4109a9a1da41d1c60
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-06-21 15:47:11 +0200

    Add running http instance alongside with https

--- src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
+++ src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
@@ -1,16 +1,38 @@
 package com.tecnalia.datausage;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.tecnalia.datausage", "com.tecnalia.datausage.api",
-		"com.tecnalia.datausage.utils", "io.dataspaceconnector.services.usagecontrol" })
+@ComponentScan(basePackages = { "com.tecnalia.datausage", "com.tecnalia.datausage.api", "com.tecnalia.datausage.utils",
+		"io.dataspaceconnector.services.usagecontrol" })
 public class Swagger2SpringBoot {
+
+	@Value("${http.port}")
+	private int httpPort;
 
 	public static void main(String[] args) throws Exception {
 		new SpringApplication(Swagger2SpringBoot.class).run(args);
-	}   
+	}
 
+	// Let's configure additional connector to enable support for both HTTP and
+	// HTTPS
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
+	}
 }
